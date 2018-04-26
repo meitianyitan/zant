96f118c410b3d745385f4eb6c13c85ec444c6c00
sosout <sosout@139.com>
2018-04-26 18:19:18 +0800

    button

--- examples/routers/button.vue
+++ examples/routers/button.vue
@@ -10,6 +10,16 @@
     <Button type="success">Success</Button>
     <Button type="warning">Warning</Button>
     <Button type="error">Error</Button>
+    <br><br>
+    <Button type="primary" shape="circle" icon="ios-search"></Button>
+    <Button type="primary" icon="ios-search">Search</Button>
+    <Button type="primary" shape="circle" icon="ios-search">Search</Button>
+    <Button type="primary" shape="circle">Circle</Button>
+    <br><br>
+    <Button type="ghost" shape="circle" icon="ios-search"></Button>
+    <Button type="ghost" icon="ios-search">Search</Button>
+    <Button type="ghost" shape="circle" icon="ios-search">Search</Button>
+    <Button type="ghost" shape="circle">Circle</Button>
   </div>
 </template>
 
